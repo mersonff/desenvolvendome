1f9aaedde4258faa2616c111ac6d618c7ebd6dfc
Emerson Freitas <mersonsi@gmail.com>
2021-08-26 17:30:19 -0300

    "test: find maximum sum algorithm test"

--- spec/find_maximum_sum_spec.rb
+++ spec/find_maximum_sum_spec.rb
@@ -2,11 +2,9 @@ require "find_maximum_sum"
 
 describe FindMaximumSum do
   it 'should find maximum sum of every three stacks correctly' do
-    stack1 = [3, 10]
-    stack2 = [4, 5]
-    stack3 = [2, 1]
-
-    # array = [-2, 0, 5, -1, 2 ]
+    stack1 = [3, 2, 1, 1, 1]
+    stack2 = [4, 3, 2]
+    stack3 = [1, 1, 4, 1]
 
     expect(FindMaximumSum.find(stack1, stack2, stack3)).to eq 5
   end
