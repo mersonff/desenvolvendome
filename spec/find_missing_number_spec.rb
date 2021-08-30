4660ffd9e8ee26d9733bf24753ab046caf5f06d0
Emerson Freitas <mersonsi@gmail.com>
2021-08-30 07:10:35 -0300

    "refactor: find missing number algorithm test refactor"

--- spec/find_missing_number_spec.rb
+++ spec/find_missing_number_spec.rb
@@ -2,7 +2,7 @@ require 'find_missing_number'
 
 describe FindMissingNumber do
   it 'should return the missing number correctly' do
-    array = [1, 2, 5, 6, 3, 8]
+    array = [1, 2, 5, 6, 3, 7]
 
     expect(FindMissingNumber.find(array)).to eq 4
   end
