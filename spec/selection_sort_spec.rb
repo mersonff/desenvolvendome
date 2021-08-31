04b1044275b914becaf968e71e3d8a08285e111f
Emerson Freitas <mersonsi@gmail.com>
2021-08-31 06:36:39 -0300

    refactor: selection sort algorithm test refactor

--- spec/selection_sort_spec.rb
+++ spec/selection_sort_spec.rb
@@ -2,9 +2,9 @@ require 'selection_sort'
 
 describe SelectionSort do
   it 'should sort a array correctly' do
-    array = [10, 8, 11, 5, 4, 9, 2]
+    array = [10, 8, 11, 5, 4, 9, 2, 100, 32, 45]
 
-    expect(SelectionSort.sort(array)).to eq [2, 4, 5, 9, 10, 11]
+    expect(SelectionSort.sort(array)).to eq [2, 4, 5, 8, 9, 10, 11, 32, 45, 100]
   end
 
 end
