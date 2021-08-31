dc902ec7ab333575e2f95904dcf11f00f2bb1bb8
Emerson Freitas <mersonsi@gmail.com>
2021-08-31 07:14:17 -0300

    refactor: quick sort algorithm test refactor

--- spec/quick_sort_spec.rb
+++ spec/quick_sort_spec.rb
@@ -4,7 +4,10 @@ describe QuickSort do
   it 'should sort a array correctly' do
     array = [10, 8, 11, 5, 4, 9, 2, 100, 32, 45]
 
-    expect(QuickSort.sort(array)).to eq [2, 4, 5, 8, 9, 10, 11, 32, 45, 100]
+    start = 0
+    final = array.length - 1
+
+    expect(QuickSort.sort(start, final, array)).to eq [2, 4, 5, 8, 9, 10, 11, 32, 45, 100]
   end
 
 end
