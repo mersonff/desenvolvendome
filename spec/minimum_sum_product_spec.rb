3b49446b38c6f2737ce107ae692afb95ee742e82
Emerson Freitas <mersonsi@gmail.com>
2021-09-01 06:59:46 -0300

    refactor: minimum sum product algorithm test refactor

--- spec/minimum_sum_product_spec.rb
+++ spec/minimum_sum_product_spec.rb
@@ -1,12 +1,12 @@
-require 'MinimumSumProduct'
+require 'minimum_sum_product'
 
 describe MinimumSumProduct do
   it 'should find the minimum product of two arrays, given k modifications correctly' do
-    a = [ 2, 3, 4, 5, 4 ]
-    b = [ 3, 4, 2, 3, 2 ]
+    a = [ 2, 3, 4, 5, 4, 7 ]
+    b = [ 3, 4, 2, 3, 2, 8 ]
     n = 5
     k = 3
 
-    expect(MinimumSumProduct.find(a, b, n, k)).eq 25
+    expect(MinimumSumProduct.find(a, b, n, k)).to eq 25
   end
 end
