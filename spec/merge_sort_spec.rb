fa1ad5c05d81a4222fa2f606693127dff182bc77
Emerson Freitas <mersonsi@gmail.com>
2021-09-01 06:33:47 -0300

    refactor: merge sort algorithm test refactor

--- spec/merge_sort_spec.rb
+++ spec/merge_sort_spec.rb
@@ -2,9 +2,9 @@ require 'merge_sort'
 
 describe MergeSort do
   it 'should sort a array correctly' do
-    array = [10, 8, 11, 5, 4, 9, 2, 100, 32, 45]
+    array = [101, 8, 11, 5, 4, 9, 2, 100, 32, 45]
 
 
-    expect(MergeSort.sort(array)).to eq [2, 4, 5, 8, 9, 10, 11, 32, 45, 100]
+    expect(MergeSort.sort(array)).to eq [2, 4, 5, 8, 9, 11, 32, 45, 100, 101]
   end
 end
