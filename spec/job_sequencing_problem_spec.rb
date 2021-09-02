a5de41ac36ac805f7bba395f94a224a5bf9821ba
Emerson Freitas <mersonsi@gmail.com>
2021-09-02 12:42:00 -0300

    refactor: job sequencing problem test

--- spec/job_sequencing_problem_spec.rb
+++ spec/job_sequencing_problem_spec.rb
@@ -2,14 +2,13 @@ require 'job_sequencing_problem'
 
 describe JobSequencingProblem do
   it 'should maximize the profit' do
-    array = [['a', 2, 100],
-           ['b', 1, 19],
-           ['c', 2, 27],
-           ['d', 1, 25],
-           ['e', 3, 15]]
+    array = [['a', 4, 20],
+           ['b', 1, 10],
+           ['c', 1, 40],
+           ['d', 1, 30]]
 
-    n = 3
+    n = 2
 
-    expect(JobSequencingProblem.calculate(array, n)).to eq 'c a e'
+    expect(JobSequencingProblem.calculate(array, n)).to eq ["c", "a"]
   end
 end
