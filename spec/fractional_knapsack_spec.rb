2dcce8608c810e6bb22381367a76c57477a58086
Emerson Freitas <mersonsi@gmail.com>
2021-09-02 21:54:39 -0300

    refactor: fractional knapsack test refactor

--- spec/fractional_knapsack_spec.rb
+++ spec/fractional_knapsack_spec.rb
@@ -2,8 +2,8 @@ require 'fractional_knapsack'
 
 describe FractionalKnapsack do
   it 'should get the maximum value' do
-    wt = [10, 40, 20, 30]
-    val = [60, 40, 100, 120]
+    wt = [10, 20, 30]
+    val = [60, 100, 120]
     capacity = 50
 
     expect(FractionalKnapsack.getMaxValue(wt, val, capacity)).to eq 240
