6be85d8bc724ce83230909e9ab432a757d4c4db3
Emerson Freitas <mersonsi@gmail.com>
2021-09-08 20:12:55 -0300

    refactor: reorder index algorithm test refactor

--- spec/reorder_index_spec.rb
+++ spec/reorder_index_spec.rb
@@ -1,13 +1,12 @@
 require 'reorder_index'
 
 describe ReorderIndex do
+  it 'should reorder the array according to indexes' do
 
-  array = [50, 40, 70, 60, 90]
-  index = [3, 0, 4, 1, 2]
-  n = array.length
+    array = [50, 40, 70, 60, 90]
+    index = [3, 0, 4, 1, 2]
+    n = array.length - 1
 
-  expect(ReorderIndex.reorder(array, index, n)).to eq "Reordered array is:
-                                                        40 60 90 50 70
-                                                        Modified Index array is:
-                                                        0 1 2 3 4"
+    expect(ReorderIndex.reorder(array, index, n)).to eq "[\"40 \", \"60 \", \"90 \", \"50 \", \"70 \"]\n[\"0 \", \"1 \", \"2 \", \"3 \", \"4 \"]"
+  end
 end
